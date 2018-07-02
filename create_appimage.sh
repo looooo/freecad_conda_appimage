ed2f23e849842169b44bbadd73a70d00367bb169
looooo <sppedflyer@gmail.com>
2018-07-02 15:20:47 +0200

    finally

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,4 +1,16 @@
 conda create -p AppDir/freecad --file freecad-spec-file.txt
+cd AppDir/freecad
+rm -rf include
+cd lib
+rm *.a
+cd ..
+mv bin bin_tmp
+mkdir bin
+cp bin_tmp/FreeCAD bin/FreeCAD
+cp bin_tmp/activate bin/  
+cp bin_tmp/conda bin/  
+cp bin_tmp/conda-env bin/
+#+ deleting some specific libraries not needed. eg.: stdc++
 ARCH=x86_64 appimagetool-x86_64.AppImage AppDir
 chmod +x FreeCAD-x86_64.AppImage
 
