058ebc066d9631d64524386001558ed2bbffed87
looooo <sppedflyer@gmail.com>
2018-09-20 07:11:17 +0200

    do not use a spec-file for creating the environment

--- create_appimage.sh
+++ create_appimage.sh
@@ -1,5 +1,27 @@
-#1 create a new environment in the AppDir with packages specified with a text-file
-conda create -p AppDir/usr --file freecad-spec-file.txt --copy
+#1 create a new environment in the AppDir
+conda create \
+    -p AppDir/usr \
+    freecad blas=*=openblas \
+    --copy \
+    --no-default-packages \
+    -c freecad/label/dev \
+    -c conda-forge \
+    -y
+
+
+## this will create a huge env. We have to find some ways to make the env smaller
+## deleting some packages explicitly?
+conda remove -p AppDir/usr --force -y \
+    ruamel_yaml conda system tk json-c llvmdev \
+    nomkl openssl readline xorg-fixesproto xorg-libsm mesalib \
+    xorg-libx11 curl gstreamer libtheora asn1crypto certifi chardet \
+    gst-plugins-base idna kiwisolver pycosat pycparser pysocks \
+    pytz sip solvespace tornado xorg-libxi xorg* cffi \
+    cycler python-dateutil setuptools cryptography pyqt soqt wheel \
+    matplotlib pip pyopenssl requests pyparsing libstdcxx-ng \
+    xz sqlite pyparsing ncurses
+
+conda list -p AppDir/usr
 
 #2 delete unnecessary stuff
 rm -rf AppDir/usr/include
@@ -7,17 +29,25 @@ find AppDir/usr -name \*.a -delete
 mv AppDir/usr/bin AppDir/usr/bin_tmp
 mkdir AppDir/usr/bin
 cp AppDir/usr/bin_tmp/FreeCAD AppDir/usr/bin/FreeCAD
-cp AppDir/usr/bin_tmp/activate AppDir/usr/bin/  
 cp AppDir/usr/bin_tmp/python AppDir/usr/bin/
-cp AppDir/usr/bin_tmp/widget.py AppDir/usr/bin/
 rm -rf AppDir/usr/bin_tmp
 #+ deleting some specific libraries not needed. eg.: stdc++
 
 #3 create the appimage
-ARCH=x86_64 ./appimagetool-x86_64.AppImage AppDir
+wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage --appimage-extract
+
+ARCH=x86_64 squashfs-root/AppRun  AppDir
 
 #4 setting rights for the appimage
 chmod +x FreeCAD-x86_64.AppImage
 
 #5 delete the created environment
 rm -rf AppDir/usr
+rm -rf squashfs-root/
+rm -rf appimagetool-x86_64.AppImage
+
+printf "############## APPIMAGE-FILE-SIZE ################\n"
+stat --printf="%s" FreeCAD-x86_64.AppImage
+printf "\n##################################################\n"
